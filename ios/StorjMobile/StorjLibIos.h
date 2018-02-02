f6a0dc8da83c0521e6904c25b1acbe832f74a3f0
Bogdan Artemenko <artemenkobogdan@gmail.com>
2018-02-02 16:27:35 +0200

    Added wrapper for storj main functions

--- ios/StorjMobile/StorjLibIos.h
+++ ios/StorjMobile/StorjLibIos.h
@@ -6,6 +6,8 @@
 //  Copyright © 2018 Facebook. All rights reserved.
 //
 @import SwiftyStorj;
+//@import SwiftyStorj.E
+//@import SwiftyStorj.StorjCallbacks;
 @import Foundation;
 #import <React/RCTBridgeModule.h>
 #import <React/RCTLog.h>
--- ios/StorjMobile/StorjLibIos.m
+++ ios/StorjMobile/StorjLibIos.m
@@ -7,40 +7,130 @@
 //
 #import "StorjLibIos.h"
 
+@interface StorjLibIos()
+@property (nonatomic) StorjWrapper * storjWrapper;
+@end
+
 @implementation StorjLibIos
 RCT_EXPORT_MODULE();
 
+
+-(StorjWrapper *)storjWrapper{
+  if(!_storjWrapper){
+    _storjWrapper = [[StorjWrapper alloc] init];
+  }
+  return _storjWrapper;
+}
+
+RCT_REMAP_METHOD(generateMnemonic,
+                 generateMnemonicWithResolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject){
+  
+  NSString *mnemonic = [_storjWrapper generateMnemonic:256];
+  if(mnemonic){
+    NSDictionary *object = @{@"isSuccess" : @YES, @"mnemonic" : mnemonic,};
+    resolve(object);
+  } else {
+    reject(STORJ_E_GENERATE_MNEMONIC,@"Unable to generate mnemonic", nil);
+  }
+}
+
+RCT_REMAP_METHOD(checkMnemonic,
+                 checkMnemonic:(NSString *) mnemonic
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+  BOOL isMnemonicValid = [_storjWrapper checkMnemonic];
+  if(isMnemonicValid){
+    resolve(@{@"isSuccess":@YES});
+  } else {
+    reject(STORJ_E_CHECK_MNEMONIC, @"Mnemonic is not correct", nil);
+  }
+}
+
+RCT_REMAP_METHOD(verifyKeys,
+                 verifyKeysWithEmail:(NSString *)email
+                 password:(NSString *)password
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject ){
+  RCTLogInfo(@"STUB");
+}
+
+RCT_REMAP_METHOD(keysExist,
+                 keysExistWithResolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject){
+  RCTLogInfo(@"STUB");
+}
+
+RCT_REMAP_METHOD(importKeys,
+                 importKeysWithEmail:(NSString *)email
+                 password: (NSString *)password
+                 mnemonic: (NSString *) mnemonic
+                 passcode: (NSString *) passcode
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject
+                 ){
+  RCTLogInfo(@"STUB");
+}
+
 RCT_REMAP_METHOD(register,
-                 login:(NSString *)login
+                 registerWithLogin:(NSString *)login
                  password:(NSString *)password
                  resolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject){
-  RCTLogInfo(@"Register clicked from rct");
-  NSLog(@"Register clicked");
-  StorjWrapper *wrapper = [[StorjWrapper alloc] init];
   RegistrationCallback *callback = [[RegistrationCallback alloc] init];
   callback.onSuccess = ^(NSString *email){
-    NSString * mnemonic = [wrapper generateMnemonic:256];
-    NSDictionary *object = @{
-                             @"isSuccess" : @YES,
-                             @"mnemonic" : mnemonic,
-//                             @"errorMessage" :
-                             };
-    resolve(object);
+    resolve(@{@"isSuccess" : @YES,});
   };
   
   callback.onError = ^(NSString *message){
-    RCTLogInfo(@"Error: %@", message);
-    //  (NSString *__strong, NSString *__strong, NSError *__strong)
-    NSDictionary *object = @{
-                             @"isSuccess" : @NO,
-//                             @"mnemonic" : mnemonic,
-                             @"errorMessage" : message
-                             };
+    NSDictionary *object = @{@"isSuccess" : @NO,@"errorMessage" : message};
     resolve(object);
     //    reject(@"regEr", message, nil);
   };
-  [wrapper registerUser:login password:password registerCallback:callback];
+  [_storjWrapper registerUser:login password:password registerCallback:callback];
+}
+
+RCT_REMAP_METHOD(createBucket,
+                 createBucketWithBucketName: (NSString *) bucketName
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+  SJBucketCreateCallBack callback = ^(NSDictionary * _Nullable bucketDictionary, NSError * _Nullable error)
+  {
+    if(bucketDictionary){
+      RCTLogInfo(@"NSDict: %@", [bucketDictionary description]);
+    }
+    if(!error && bucketDictionary)
+    {
+      resolve(bucketDictionary);
+    } else
+    {
+      reject(STORJ_E_CREATE_BUCKET, @"Unable to create bucket", nil);
+    }
+  };
+  
+  [_storjWrapper createBucket:bucketName withCompletion:callback];
+}
+
+RCT_REMAP_METHOD(deleteBucket,
+                 deleteBucketWithBucketName: (NSString *) bucketName
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject){
+  RCTLogInfo(@"STUB");
+}
+
+RCT_REMAP_METHOD(getBuckets,
+                 getBucketsWithResolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+  [_storjWrapper getBucketListWithCompletion:^(NSArray<NSDictionary *> * _Nullable bucketsArray, NSError * _Nullable error) {
+    if(!error && bucketsArray){
+      //        resolve
+    } else {
+      //      reject
+    }
+  }];
 }
 
 @end
