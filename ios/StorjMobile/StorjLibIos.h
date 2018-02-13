9bff071630dfa033282f0ba5eca5ae48599a6e61
Bogdan Artemenko <artemenkobogdan@gmail.com>
2018-02-13 19:48:20 +0200

    StorjLib wrapper. Implemented Registration, Mnemonic, Bucket operations.

--- ios/StorjMobile/StorjLibIos.h
+++ ios/StorjMobile/StorjLibIos.h
@@ -5,12 +5,12 @@
 //  Created by Barterio on 1/15/18.
 //  Copyright © 2018 Facebook. All rights reserved.
 //
-@import SwiftyStorj;
-//@import SwiftyStorj.E
-//@import SwiftyStorj.StorjCallbacks;
 @import Foundation;
+
+
 #import <React/RCTBridgeModule.h>
 #import <React/RCTLog.h>
 
 @interface StorjLibIos : NSObject<RCTBridgeModule>
+//@property (nonatomic) StorjWrapper *storjWrapper;
 @end
