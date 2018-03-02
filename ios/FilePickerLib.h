b2d3ed882f2579e9200e0f97a02452c8d3999784
Bogdan Artemenko <artemenkobogdan@gmail.com>
2018-03-02 13:08:40 +0200

    FilePicker draft

---
+++ ios/FilePickerLib.h
@@ -0,0 +1,19 @@
+//
+//  FilePickerLib.h
+//  StorjMobile
+//
+//  Created by Barterio on 2/27/18.
+//  Copyright © 2018 Storj. All rights reserved.
+//
+
+@import Foundation;
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTLog.h>
+#import <UIKit/UIKit.h>
+
+@interface FilePickerLib : NSObject<RCTBridgeModule,UINavigationControllerDelegate,
+                                    UIActionSheetDelegate, UIImagePickerControllerDelegate>
+
+
+@end
