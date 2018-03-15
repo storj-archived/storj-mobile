ddd775f94388701db43fc2e35e25d0faa82dd729
Yaroslav Vorobiov <yar.vorobiov@gmail.com>
2018-03-15 15:32:21 +0200

    Small sync updates

--- android/app/src/main/java/storjlib/services/SynchronizationJobService.java
+++ android/app/src/main/java/storjlib/services/SynchronizationJobService.java
@@ -13,11 +13,14 @@ import java.io.File;
 import java.util.List;
 
 import storjlib.Models.UploadingFileModel;
+import storjlib.Responses.Response;
 import storjlib.dataProvider.DatabaseFactory;
+import storjlib.dataProvider.Dbo.BucketDbo;
 import storjlib.dataProvider.Dbo.FileDbo;
 import storjlib.dataProvider.Dbo.UploadingFileDbo;
 import storjlib.dataProvider.contracts.FileContract;
 import storjlib.dataProvider.contracts.UploadingFileContract;
+import storjlib.dataProvider.repositories.BucketRepository;
 import storjlib.dataProvider.repositories.FileRepository;
 import storjlib.dataProvider.repositories.UploadingFilesRepository;
 
@@ -34,7 +37,7 @@ public class SynchronizationJobService extends JobService {
         mBackgroundTask = new AsyncTask() {
             @Override
             protected Object doInBackground(Object[] objects) {
-                syncFolder("/storage/emulated/0/Movies", "12f19c568912d709e51feb2f");
+                syncFolder("/storage/emulated/0/Download", "1f300c24ac1ed2d706c10bdb");
                 /*mNotificationService.init(SynchronizationJobService.this);
                 mNotificationService.notifyPriorityHigh(21321, "hello");*/
                 return null;
@@ -59,6 +62,7 @@ public class SynchronizationJobService extends JobService {
     }
 
     private void uploadFile(String bucketId, String uri) {
+        Log.d(DEBUG_TAG, "sync: " + "sending new intent for " + uri);
         Intent uploadIntent = new Intent(this, UploadService.class);
         uploadIntent.setAction(UploadService.ACTION_UPLOAD_FILE);
         uploadIntent.putExtra(UploadService.PARAMS_BUCKET_ID, bucketId);
@@ -72,6 +76,7 @@ public class SynchronizationJobService extends JobService {
         File folder = new File(folderUri);
 
         if(!folder.exists() || !folder.isDirectory()) {
+            Log.d(DEBUG_TAG, "sync: " + "File not exist or is directory");
             return;
         }
 
@@ -80,9 +85,19 @@ public class SynchronizationJobService extends JobService {
         try(SQLiteDatabase db = new DatabaseFactory(this, null).getReadableDatabase()) {
             FileRepository fileRepo = new FileRepository(db);
             UploadingFilesRepository uploadFileRepo = new UploadingFilesRepository(db);
+            BucketRepository bucketRepo = new BucketRepository(db);
+
+            BucketDbo bucketDbo = bucketRepo.get(bucketId);
+
+            if(bucketDbo == null) {
+                Log.d(DEBUG_TAG, "sync: " + "No bucketDbo");
+                return;
+            }
 
             for(File file : files) {
+                Log.d(DEBUG_TAG, "sync: " + "File, name: " + file.getName());
                 if(file.isDirectory()) {
+                    Log.d(DEBUG_TAG, "sync: " + "File is directory continue");
                     continue;
                 }
 
