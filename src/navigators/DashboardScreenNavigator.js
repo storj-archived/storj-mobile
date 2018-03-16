edfca2c45b341b2d5b3c75ccb99f16f2e544cacf
Yaroslav Vorobiov <yar.vorobiov@gmail.com>
2018-03-16 18:00:31 +0200

    Dashboard fix

--- src/navigators/DashboardScreenNavigator.js
+++ src/navigators/DashboardScreenNavigator.js
@@ -8,7 +8,7 @@ const DashboardScreenNavigator = StackNavigator(
             screen: DashboardScreen,
             routeName: 'DashboardDefaultScreen'
         },
-        FilesScreen: {
+        DashboardFilesScreen: {
             screen: FilesScreen,
             routeName: 'DashboardFilesScreen'
         }
