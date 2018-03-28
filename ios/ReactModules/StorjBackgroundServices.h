6d013ca5b011373890cbd09c171976d4ff4b529a
Bogdan Artemenko <artemenkobogdan@gmail.com>
2018-03-28 14:35:12 +0300

    Added fileExternal operations, uploadService

---
+++ ios/ReactModules/StorjBackgroundServices.h
@@ -0,0 +1,45 @@
+//
+//  StorjBackgroundServices.h
+//  StorjMobile
+//
+//  Created by Barterio on 3/19/18.
+//  Copyright © 2018 Storj. All rights reserved.
+//
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+#import <React/RCTLog.h>
+#import "DatabaseFactory.h"
+#import "BaseRepository.h"
+#import "BucketRepository.h"
+#import "FileRepository.h"
+#import "UploadFileRepository.h"
+#import "FileDeleteModel.h"
+#import "UploadFileDbo.h"
+#import "UploadFileModel.h"
+#import "UploadFileProgressModel.h"
+#import "BucketModel.h"
+#import "ListResponse.h"
+#import "SingleResponse.h"
+#import "MethodHandler.h"
+#import "DictionaryUtils.h"
+#import "FileUtils.h"
+#import "EventNames.h"
+#import "PromiseHandler.h"
+
+@import StorjIOS;
+
+@interface StorjBackgroundServices : RCTEventEmitter<RCTBridgeModule>
+
+@property (nonatomic, strong) FMDatabase *_database;
+@property (nonatomic, strong) BucketRepository *_bucketRepository;
+@property (nonatomic, strong) FileRepository *_fileRepository;
+@property (nonatomic, strong) UploadFileRepository *_uploadFileRepository;
+
+@property (nonatomic, strong) PromiseHandler *_mainOperationsPromise;
+@property (nonatomic, strong) PromiseHandler *_uploadOperationsPromise;
+@property (nonatomic, strong) PromiseHandler *_downloadOperationsPromise;
+
+@property (nonatomic, strong) StorjWrapper *_storjWrapper;
+
+@end
---
+++ ios/utils/FileUtils.h
@@ -0,0 +1,14 @@
+//
+//  FileUtils.h
+//  StorjMobile
+//
+//  Created by Barterio on 3/28/18.
+//  Copyright © 2018 Storj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FileUtils : NSObject
++(NSNumber *) getFileSizeWithPath: (NSString *)filePath;
++(NSString *) getFileNameWithPath: (NSString *)filePath;
+@end
