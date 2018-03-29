6b9001867a4f75cbc049c9fbcad166af8074a905
chau <cchau14@gmail.com>
2018-03-29 12:12:46 +0300

    [FIXED] bug with stacking dashboard screens on multiple navigation

--- src/reducers/navigation/dashboardScreenNavReducer.js
+++ src/reducers/navigation/dashboardScreenNavReducer.js
@@ -3,9 +3,16 @@ import DashboardScreenNavigator from '../../navigators/DashboardScreenNavigator'
 const initialState = DashboardScreenNavigator.router.getStateForAction(DashboardScreenNavigator.router.getActionForPathAndParams('DashboardDefaultScreen'));
 
 export default function bucketsScreenNavReducer(state = initialState, action) { 
-    let nextState = DashboardScreenNavigator.router.getStateForAction(action, state);
 
+    let nextState = DashboardScreenNavigator.router.getStateForAction(action, state);
+    
     try {
+        let length = nextState.routes.length;
+        if(length > 2 && nextState.routes[length-1].routeName === nextState.routes[length-2].routeName) 
+        {
+            nextState = state;
+        }
+
         DashboardScreenNavigator.router.getPathAndParamsForState(nextState);
     } catch(e) {
         console.log(e.message);
