5a1ab50fab6c113255ef204b4afcbe89afd3d5ea
Bogdan Artemenko <artemenkobogdan@gmail.com>
2018-03-29 11:32:36 +0300

    Added SHA256 React Module

--- src/utils/sha256.js
+++ src/utils/sha256.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const {NativeModules} = require('react-native')
-const sha256Module = NativeModules.sha256;
+const {NativeModules, Platform} = require('react-native')
+const sha256Module = Platform.OS === "android" ? NativeModules.sha256 : NativeModules.Sha256ModuleIOS;
 
-export function sha256(data) {    
+export function sha256(data) {
+
   return sha256Module.sha256(data);
 }
