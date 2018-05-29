743ed40917b3b30571a47814caca4f45d5b8d52a
Bogdan Artemenko <artemenkobogdan@gmail.com>
2018-05-29 16:22:05 +0300

    [iOS] Fixed NPE on User password at Sha256Module.

--- ios/ReactModules/Sha256Module.m
+++ ios/ReactModules/Sha256Module.m
@@ -22,16 +22,21 @@ RCT_EXPORT_METHOD(sha256: (NSString *) data
                   rejecter: (RCTPromiseRejectBlock) reject)
 
 {
+  if(!data)
+  {
+    resolve(@"");
+    return;
+  }
   const char* str = [data UTF8String];
   unsigned char result[CC_SHA256_DIGEST_LENGTH];
   CC_SHA256(str, strlen(str), result);
-  
+
   NSMutableString *ret = [NSMutableString stringWithCapacity:CC_SHA256_DIGEST_LENGTH*2];
   for(int i = 0; i<CC_SHA256_DIGEST_LENGTH; i++)
   {
     [ret appendFormat:@"%02x",result[i]];
   }
-  
+//  resolve(@"");
   resolve(ret);
 }
 
