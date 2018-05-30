545891c177a52f29425e2337ef245ad239b4321b
Bogdan Artemenko <artemenkobogdan@gmail.com>
2018-05-30 13:16:53 +0300

    [iOS] added file deletion on file upload success and error.

--- ios/loaders/STUploader.m
+++ ios/loaders/STUploader.m
@@ -27,181 +27,188 @@
 #import "Logger.h"
 
 @implementation STUploader
-  {
-    UploadFileDbo *_uploadFileDbo;
-    NSString *_bucketId;
-    NSString *_localPath;
-    NSString *_fileName;
-    
-    SJFileUploadCallback *_innerUploadFileCallback;
-    STFileUploadCallback *_notifyUploadFileCallback;
-    
-    double _uploadProgress;
-    
-    StorjWrapper *_storjWrapper;
-    
-    UploadFileRepository *_uploadFileRepository;
-    FileRepository *_fileRepository;
-  }
+{
+  UploadFileDbo *_uploadFileDbo;
+  NSString *_bucketId;
+  NSString *_localPath;
+  NSString *_fileName;
   
+  SJFileUploadCallback *_innerUploadFileCallback;
+  STFileUploadCallback *_notifyUploadFileCallback;
+  
+  double _uploadProgress;
+  
+  StorjWrapper *_storjWrapper;
+  
+  UploadFileRepository *_uploadFileRepository;
+  FileRepository *_fileRepository;
+}
+
 -(instancetype) initWithBucketId: (NSString *) bucketId
                        localPath: (NSString *) localPath
                         fileName: (NSString *) fileName
                 callbackNotifier: (STFileUploadCallback *) fileUploadCallback
+{
+  if(self = [super init])
   {
-    if(self = [super init])
-    {
-      _bucketId = bucketId;
-      _localPath = localPath;
-      _fileName = fileName;
-      _notifyUploadFileCallback = fileUploadCallback;
-    }
-    return self;
+    _bucketId = bucketId;
+    _localPath = localPath;
+    _fileName = fileName;
+    _notifyUploadFileCallback = fileUploadCallback;
   }
-  
+  return self;
+}
+
 -(UploadFileRepository *) _uploadFileRepository
+{
+  if(!_uploadFileRepository)
   {
-    if(!_uploadFileRepository)
-    {
-      _uploadFileRepository = [[UploadFileRepository alloc] init];
-    }
-  
-    return _uploadFileRepository;
+    _uploadFileRepository = [[UploadFileRepository alloc] init];
   }
   
+  return _uploadFileRepository;
+}
+
 -(FileRepository *) _fileRepository
+{
+  if(!_fileRepository)
   {
-    if(!_fileRepository)
-    {
-      _fileRepository = [[FileRepository alloc] init];
-    }
-    
-    return _fileRepository;
+    _fileRepository = [[FileRepository alloc] init];
   }
   
-  -(StorjWrapper *) _storjWrapper
+  return _fileRepository;
+}
+
+-(StorjWrapper *) _storjWrapper
+{
+  if(!_storjWrapper)
   {
-    if(!_storjWrapper)
-    {
-      _storjWrapper = [[StorjWrapperSingletone sharedStorjWrapper]storjWrapper];
-    }
-    return _storjWrapper;
+    _storjWrapper = [[StorjWrapperSingletone sharedStorjWrapper]storjWrapper];
   }
-  
+  return _storjWrapper;
+}
+
 -(BOOL) isUploadValid
-  {
-    return _bucketId && _bucketId.length > 0
-    && _localPath && _localPath.length;
-  }
-  
+{
+  return _bucketId && _bucketId.length > 0
+  && _localPath && _localPath.length;
+}
+
 -(void) startUpload
-  {
-    NSNumber *fileSize = [FileUtils getFileSizeWithPath:_localPath];
-    NSString *fileName = [FileUtils getFileNameWithPath:_localPath];
+{
+  NSNumber *fileSize = [FileUtils getFileSizeWithPath:_localPath];
+  NSString *fileName = [FileUtils getFileNameWithPath:_localPath];
+  
+  _uploadFileDbo = [[UploadFileDbo alloc] initWithFileHandle:0
+                                                    progress:0
+                                                        size:[fileSize longValue]
+                                                    uploaded:0
+                                                        name:fileName
+                                                         uri:_localPath
+                                                    bucketId:_bucketId];
+  
+  _innerUploadFileCallback = [[SJFileUploadCallback alloc] init];
+  
+  _innerUploadFileCallback.onProgress = ^(NSString *fileId, double progress, double uploadedBytes, double totalBytes) {
+    if([_uploadFileDbo fileHandle] == 0 || progress == 0){
+      return;
+    }
     
-    _uploadFileDbo = [[UploadFileDbo alloc] initWithFileHandle:0
-                                                          progress:0
-                                                              size:[fileSize longValue]
-                                                          uploaded:0
-                                                              name:fileName
-                                                               uri:_localPath
-                                                          bucketId:_bucketId];
+    if(progress - _uploadProgress > 0.02){
+      _uploadProgress = progress;
+    }
     
-    _innerUploadFileCallback = [[SJFileUploadCallback alloc] init];
+    if(_uploadProgress != progress){
+      return;
+    }
     
-    _innerUploadFileCallback.onProgress = ^(NSString *fileId, double progress, double uploadedBytes, double totalBytes) {
-      if([_uploadFileDbo fileHandle] == 0 || progress == 0){
-        return;
-      }
-      
-      if(progress - _uploadProgress > 0.02){
-        _uploadProgress = progress;
-      }
-      
-      if(_uploadProgress != progress){
-        return;
-      }
-      
-      [_uploadFileDbo set_progress: _uploadProgress];
-      [_uploadFileDbo set_uploaded: uploadedBytes];
-      
-      UploadFileModel * fileModel =[[UploadFileModel alloc] initWithUploadFileDbo:_uploadFileDbo];
-      Response *response = [[self _uploadFileRepository] updateByModel:fileModel];
-      if(_notifyUploadFileCallback)
-      {
-        [_notifyUploadFileCallback uploadProgressWithFileHandle: [_uploadFileDbo fileHandle]
-                                                  uploadProgres: _uploadProgress
-                                                    uploadBytes: uploadedBytes];
-      }
-    };
+    [_uploadFileDbo set_progress: _uploadProgress];
+    [_uploadFileDbo set_uploaded: uploadedBytes];
     
-    _innerUploadFileCallback.onError = ^(int errorCode, NSString * _Nullable errorMessage) {
-      NSString *dboId = [NSString stringWithFormat:@"%ld", [_uploadFileDbo fileHandle]];
-      Response *deleteResponse = [[self _uploadFileRepository] deleteById:dboId];
-      
-      [_notifyUploadFileCallback errorWithFileHandle:[_uploadFileDbo fileHandle]
-                                           errorCode:errorCode
-                                        errorMessage:errorMessage];
-    };
-    
-    _innerUploadFileCallback.onSuccess = ^(SJFile * file){
-      ThumbnailProcessor *thumbnailProcessor = [[ThumbnailProcessor alloc]
-                                                initThumbnailProcessorWithFileRepository:[self _fileRepository]];
-      NSString *thumbnail = nil;
-      // Due to the fact that StorjLib returns mimeType: applicatio9n/octet-stream, we are trying to
-      // generate thumbnail image and save it to local database
-      SingleResponse *thumbnailGenerationResponse = [thumbnailProcessor getThumbnailWithFilePath:_localPath];
-      if([thumbnailGenerationResponse isSuccess])
-      {
-        thumbnail = [thumbnailGenerationResponse getResult];
-      }
-      NSFileManager *fileManager = [NSFileManager defaultManager];
-      [_localPath stringByStandardizingPath];
-      NSString *filePath = [_localPath stringByReplacingOccurrencesOfString: @"file://"
-                                                                 withString: @""];
-      if([fileManager fileExistsAtPath:filePath])
-      {
-        NSError *error;
-        [fileManager removeItemAtPath: filePath error: &error];
-      }
-      
-      FileModel *fileModel = [[FileModel alloc] initWithSJFile:file
-                                                       starred:NO
-                                                        synced:NO
-                                                 downloadState:0
-                                                    fileHandle:[_uploadFileDbo fileHandle]
-                                                       fileUri:@""
-                                                     thumbnail:thumbnail];
-      
-      [fileModel set_name:[_uploadFileDbo name]];
-      
-      Response *deleteUploadFileResponse = [[self _uploadFileRepository]
-                                            deleteById:[NSString stringWithFormat:@"%ld",
-                                                        [_uploadFileDbo fileHandle]]];
-      Response *insertFileResponse = [[self _fileRepository] insertWithModel:fileModel];
-      
-      [_notifyUploadFileCallback uploadCompleteWithFileHandle:[_uploadFileDbo fileHandle] fileId:[fileModel _fileId]];
-    };
-    
-    long fileRef = [[self _storjWrapper] uploadFile:_localPath
-                                           toBucket:_bucketId
-                                     withCompletion:_innerUploadFileCallback];
-    @synchronized (_uploadFileDbo)
+    UploadFileModel * fileModel =[[UploadFileModel alloc] initWithUploadFileDbo:_uploadFileDbo];
+    Response *response = [[self _uploadFileRepository] updateByModel:fileModel];
+    if(_notifyUploadFileCallback)
     {
-      [_uploadFileDbo set_fileHandle:fileRef];
-      UploadFileModel *fileModel = [[UploadFileModel alloc] initWithUploadFileDbo:_uploadFileDbo];
-      
-      Response *insertResponse = [[self _uploadFileRepository] insertWithModel:fileModel];
-      if([insertResponse isSuccess])
-      {
-        [_notifyUploadFileCallback uploadStartWithFileHandle:[_uploadFileDbo fileHandle]];
-      }
+      [_notifyUploadFileCallback uploadProgressWithFileHandle: [_uploadFileDbo fileHandle]
+                                                uploadProgres: _uploadProgress
+                                                  uploadBytes: uploadedBytes];
+    }
+  };
+  
+  _innerUploadFileCallback.onError = ^(int errorCode, NSString * _Nullable errorMessage) {
+    NSString *dboId = [NSString stringWithFormat:@"%ld", [_uploadFileDbo fileHandle]];
+    Response *deleteResponse = [[self _uploadFileRepository] deleteById:dboId];
+    
+    [self deleteFile];
+    
+    [_notifyUploadFileCallback errorWithFileHandle:[_uploadFileDbo fileHandle]
+                                         errorCode:errorCode
+                                      errorMessage:errorMessage];
+  };
+  
+  _innerUploadFileCallback.onSuccess = ^(SJFile * file){
+    ThumbnailProcessor *thumbnailProcessor = [[ThumbnailProcessor alloc]
+                                              initThumbnailProcessorWithFileRepository:[self _fileRepository]];
+    NSString *thumbnail = nil;
+    // Due to the fact that StorjLib returns mimeType: applicatio9n/octet-stream, we are trying to
+    // generate thumbnail image and save it to local database
+    SingleResponse *thumbnailGenerationResponse = [thumbnailProcessor getThumbnailWithFilePath:_localPath];
+    if([thumbnailGenerationResponse isSuccess])
+    {
+      thumbnail = [thumbnailGenerationResponse getResult];
+    }
+    
+    FileModel *fileModel = [[FileModel alloc] initWithSJFile:file
+                                                     starred:NO
+                                                      synced:NO
+                                               downloadState:0
+                                                  fileHandle:[_uploadFileDbo fileHandle]
+                                                     fileUri:@""
+                                                   thumbnail:thumbnail];
+    
+    [fileModel set_name:[_uploadFileDbo name]];
+    
+    Response *deleteUploadFileResponse = [[self _uploadFileRepository]
+                                          deleteById:[NSString stringWithFormat:@"%ld",
+                                                      [_uploadFileDbo fileHandle]]];
+    Response *insertFileResponse = [[self _fileRepository] insertWithModel:fileModel];
+    
+    [self deleteFile];
+    
+    [_notifyUploadFileCallback uploadCompleteWithFileHandle:[_uploadFileDbo fileHandle] fileId:[fileModel _fileId]];
+  };
+  
+  long fileRef = [[self _storjWrapper] uploadFile:_localPath
+                                         toBucket:_bucketId
+                                   withCompletion:_innerUploadFileCallback];
+  @synchronized (_uploadFileDbo)
+  {
+    [_uploadFileDbo set_fileHandle:fileRef];
+    UploadFileModel *fileModel = [[UploadFileModel alloc] initWithUploadFileDbo:_uploadFileDbo];
+    
+    Response *insertResponse = [[self _uploadFileRepository] insertWithModel:fileModel];
+    if([insertResponse isSuccess])
+    {
+      [_notifyUploadFileCallback uploadStartWithFileHandle:[_uploadFileDbo fileHandle]];
     }
   }
-  
--(BOOL) isUploadComplete
+}
+-(void) deleteFile
+{
+  NSFileManager *fileManager = [NSFileManager defaultManager];
+  [_localPath stringByStandardizingPath];
+  NSString *filePath = [_localPath stringByReplacingOccurrencesOfString: @"file://"
+                                                             withString: @""];
+  if([fileManager fileExistsAtPath:filePath])
   {
-    return NO;
+    NSError *error;
+    [fileManager removeItemAtPath: filePath error: &error];
   }
-  
-  @end
+}
+
+-(BOOL) isUploadComplete
+{
+  return NO;
+}
+
+@end
